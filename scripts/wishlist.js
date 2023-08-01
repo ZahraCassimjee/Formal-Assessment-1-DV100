715995d3af59c0145aed76c177333e1aa0d23803
ZahraCassimjee <231131@virtualwindow.co.za>
2023-08-01 21:39:54 +0200

    start scrip

---
+++ scripts/wishlist.js
@@ -0,0 +1,32 @@
+const wishplants = [
+     
+    {"pic": "plant1.png",
+    "pName": "Fikus Tree",
+    "remove": "-",
+    },
+
+    {"pic": "plant2.png",
+    "pName": "White Sprite Succulent",
+    "remove": "-",
+    },
+
+    {"pic": "plant3.png",
+    "pName": "Snake Plant",
+    "remove": "-",
+    },
+
+    {"pic": "plant4.png",
+    "pName": "Parlour Palm",
+    "remove": "-",
+    },
+
+    {"pic": "plant5.png",
+    "pName": "Japanese Maple",
+    "remove": "-",
+    },
+]
+
+$(document).ready(function(){
+
+    loadWishlist();
+});
